85381cc84e62cbd0be53ee5074d2a191f59cb52b
zshhhh <zhangsh0711@gmail.com>
2016-04-03 16:20:03 -0400

    ast

---
+++ ast.ml
@@ -0,0 +1,26 @@
+type op = Add | Sub | Mult | Div | Mod | Equal | AddEqual | SubEqual | MultEqual | DivEqual | ModEqual | Neq | Less | Leq | Greater | Geq | And | Or | Is | Xor | ShiftLeft | ShiftRight | At
+type uop = Neg | Not
+type typ = Int | Bool | Null | Double | Char | String | IntArray | CharArray | BoolArray | DoubleArray | StringArray | Game | Player | Sprite | Map
+type bind = typ * string
+
+type expr = Literal of int            | BoolLit of bool
+          | Id of string              | Noexpr
+          | Binop of expr * op * expr | Unop of uop * expr
+          | Assign of string * expr   | Call of string * expr
+
+type stmt = Block of stmt list        | Expr of expr
+          | If of expr * stmt         | Else of stmt
+          | Elif of expr * stmt       
+          | For of expr * expr * expr * stmt
+          | While of expr * stmt      | Return of expr
+          | Break                     | Continue
+
+type func_decl = {
+	typ      : typ;
+	fname    : string;
+	formals  : bind list;
+	locals   : bind list;
+	body     : stmt list;
+}
+
+type program = bind list
