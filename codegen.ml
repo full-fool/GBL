a2324b25ab7a48ee76da8839106b5edd8c18208f
caoy11 <YeCao@dyn-160-39-192-28.dyn.columbia.edu>
2016-04-04 18:37:14 -0400

    fix bug

--- codegen.ml
+++ codegen.ml
@@ -112,7 +112,7 @@ let translate (globals, functions) =
       | A.Assign (s, e) -> let e' = expr builder e in
 	                   ignore (L.build_store e' (lookup s) builder); e'
       | A.Call ("print", [e])  ->
-      L.build_call put_func (expr builder e) "" builder
+      L.build_call put_func [| expr builder e |] "" builder
       | A.Call ("printb", [e]) ->
 	    L.build_call printf_func [| int_format_str ; (expr builder e) |] "printf" builder
       | A.Call (f, act) ->
