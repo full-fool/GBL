a336e2a57294bfb01e5aaede0441f21a9bbff470
caoy11 <YeCao@dyn-160-39-192-28.dyn.columbia.edu>
2016-04-04 18:44:51 -0400

    test

--- codegen.ml
+++ codegen.ml
@@ -111,8 +111,6 @@ let translate (globals, functions) =
       | A.Not     -> L.build_not) e' "tmp" builder
       | A.Assign (s, e) -> let e' = expr builder e in
 	                   ignore (L.build_store e' (lookup s) builder); e'
-      | A.Call ("print", [e]) -> L.build_call put_func [| expr builder e |] "" builder
-      | A.Call ("printb", [e]) -> L.build_call printf_func [| int_format_str ; (expr builder e) |] "printf" builder
       | A.Call (f, act) ->
          let (fdef, fdecl) = StringMap.find f function_decls in
 	 let actuals = List.rev (List.map (expr builder) (List.rev act)) in
