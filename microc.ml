f72fdfa423553001361acdda06312f55507c65a1
Sihao Zhang <zhangsh0711@gmail.com>
2016-04-04 20:55:47 -0400

    Update microc.ml

--- microc.ml
+++ microc.ml
@@ -5,13 +5,15 @@ type action = Ast | LLVM_IR | Compile
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-l", LLVM_IR);  (* Generate LLVM, don't check *)
+    List.assoc Sys.argv.(1) [ ("-a", Ast);
+                              ("-l", LLVM_IR);  (* Generate LLVM, don't check *)
 			      ("-c", Compile) ] (* Generate, check LLVM IR *)
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.token lexbuf in
   match action with
-    LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
+    Ast -> print_string (Ast.string_of_program ast)
+  | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
   | Compile -> let m = Codegen.translate ast in
     Llvm_analysis.assert_valid_module m;
     print_string (Llvm.string_of_llmodule m)
