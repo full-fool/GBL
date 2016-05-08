468330836d1c94ce0d0da08e9ffda4a070d06830
zst19930322 <sz2539@columbia.edu>
2016-05-07 23:48:13 -0400

    add gobang.py

--- gbloutput.py
+++ gbloutput.py
@@ -57,18 +57,6 @@ class Gobang:
         self.PlayerNumber = None
         self.GridNum = None
         self.MapSize = [ None ] * 2
-        self.WithAI = None
-        self.NextSpriteID = None
-        self.NextPlayerID = None
-        self.FormerPosition = [ None ] * 2
-        self.FormerId = None
-        self.SpriteOwnerId = [ None ] * 1000
-        self.SpriteId = [ None ] * 1000
-        self.PlayerName = [ None ] * 10
-        self.PlayerId = [ None ] * 10
-        self.PlayerNumber = None
-        self.GridNum = None
-        self.MapSize = [ None ] * 2
         pass
     def initialize(self,MapS,InputPlayerNum,InputPlayerID,InputPlayerName,InputwithAI):
         self.MapSize[0]=MapS[0]
