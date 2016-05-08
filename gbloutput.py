d9077b03efd523d5693485fb6a5e56a00c687f33
zst19930322 <sz2539@columbia.edu>
2016-05-07 21:01:06 -0400

    update

--- gbloutput.py
+++ gbloutput.py
@@ -4,17 +4,14 @@ class UserMain:
     def main(self):
         mygame = Gobang()
         MapS = [ None ] * 2
-
         MapS[0]=15
         MapS[1]=15
         GridNumber = (MapS[0]) * (MapS[1])
         InputPlayerNumber = 2
         InputPlayerId = [ None ] * InputPlayerNumber
-
         InputPlayerId[0]=0
         InputPlayerId[1]=1
         InputPlayerName = [ None ] * InputPlayerNumber
-
         InputPlayerName[0]="Cuidiao"
         InputPlayerName[1]="Xicao"
         InputwithAI = False
@@ -24,19 +21,14 @@ class UserMain:
             print("This is the turn of player")
             print(mygame.NextPlayerID)
             InputPosition = [ None ] * 2
-
             if ((InputwithAI) and ((mygame.NextPlayerID) != (0))):
                 rposition = None
-
                 rposition = gobanai.returnposition()
                 InputPosition[0]=(rposition) / (mygame.MapSize[0])
                 InputPosition[1]=(rposition) % (mygame.MapSize[0])
             else:
                 legal = False
                 while (not (legal)):
-                    tmp = None
-
-                    tmp = input()
                     InputPosition[0]=input()
                     InputPosition[1]=input()
                     legal = mygame.isLegal(InputPosition)
@@ -65,6 +57,38 @@ class Gobang:
         self.PlayerNumber = None
         self.GridNum = None
         self.MapSize = [ None ] * 2
+
+        pass
+    def initialize(self,MapS,InputPlayerNum,InputPlayerID,InputPlayerName,InputwithAI):
+        self.MapSize[0]=MapS[0]
+        self.MapSize[1]=MapS[1]
+        self.GridNum = (self.MapSize[0]) * (self.MapSize[1])
+        self.PlayerNumber = InputPlayerNum
+        self.withAI = InputwithAI
+        i = None
+
+        i = 0
+        while ((i) < (self.PlayerNumber)):
+            self.PlayerId[i]=InputPlayerID[i]
+            self.PlayerName[i]=InputPlayerName[i]
+            i = (i) + (1)
+        i = None
+
+        j = None
+
+        i = 0
+        while ((i) < (self.MapSize[0])):
+            j = 0
+            while ((j) < (self.MapSize[1])):
+                self.SpriteId[((i) * (self.MapSize[0])) + (j)]= - (1)
+                self.SpriteOwnerId[((i) * (self.MapSize[0])) + (j)]= - (1)
+                j = (j) + (1)
+            i = (i) + (1)
+        self.FormerId =  - (1)
+        self.FormerPosition[0]= - (1)
+        self.FormerPosition[1]= - (1)
+        self.NextPlayerID = 0
+        self.NextSpriteID = 0
         pass
     def update(self,InputPosition):
         self.SpriteId[((InputPosition[0]) * (self.MapSize[0])) + (InputPosition[1])]=self.NextSpriteID
@@ -88,7 +112,6 @@ class Gobang:
     def win(self):
         PlayerSprite = self.FormerId
         position = [ None ] * 2
-
         position[0]=self.FormerPosition[0]
         position[1]=self.FormerPosition[1]
         if (((position[0]) - (4)) > (0)):
@@ -108,11 +131,8 @@ class Gobang:
         else:
             down = (position[1]) + (4)
         i = None
-
         j = None
-
         count = None
-
         PreSprite =  - (1)
         count = 0
         j = left
@@ -148,9 +168,7 @@ class Gobang:
         PreSprite =  - (1)
         count = 0
         leftup = [ None ] * 2
-
         leftupDistance = None
-
         leftupDistance = 4
         if ((position[0]) < (4)):
             leftupDistance = position[0]
@@ -159,9 +177,7 @@ class Gobang:
         leftup[0]=(position[0]) - (leftupDistance)
         leftup[1]=(position[1]) - (leftupDistance)
         rightdown = [ None ] * 2
-
         rightdownDistance = None
-
         rightdownDistance = 4
         if ((((self.MapSize[0]) - (1)) - (position[0])) < (4)):
             rightdownDistance = ((self.MapSize[0]) - (1)) - (position[0])
@@ -170,7 +186,6 @@ class Gobang:
         rightdown[0]=(position[0]) + (rightdownDistance)
         rightdown[1]=(position[1]) + (rightdownDistance)
         curposition = [ None ] * 2
-
         i = 0
         while ((i) <= ((rightdown[0]) - (leftup[0]))):
             curposition[0]=(leftup[0]) + (i)
@@ -190,7 +205,6 @@ class Gobang:
         PreSprite =  - (1)
         count = 0
         leftdown = [ None ] * 2
-
         leftdownDistance = 4
         if ((position[0]) < (4)):
             leftdownDistance = position[0]
@@ -199,7 +213,6 @@ class Gobang:
         leftdown[0]=(position[0]) - (leftdownDistance)
         leftdown[1]=(position[1]) + (leftdownDistance)
         rightup = [ None ] * 2
-
         rightupDistance = 4
         if ((((self.MapSize[0]) - (1)) - (position[0])) < (4)):
             rightupDistance = ((self.MapSize[0]) - (1)) - (position[0])
@@ -208,7 +221,6 @@ class Gobang:
         rightup[0]=(position[0]) + (rightupDistance)
         rightup[1]=(position[1]) - (rightupDistance)
         curposition = [ None ] * 2
-
         i = 0
         while ((i) <= ((rightup[0]) - (leftdown[0]))):
             curposition[0]=(leftdown[0]) + (i)
@@ -234,16 +246,13 @@ class Gobang:
         self.PlayerNumber = InputPlayerNum
         withAI = InputwithAI
         i = None
-
         i = 0
         while ((i) < (self.PlayerNumber)):
             self.PlayerId[i]=InputPlayerID[i]
             self.PlayerName[i]=InputPlayerName[i]
             i = (i) + (1)
         i = None
-
         j = None
-
         i = 0
         while ((i) < (self.MapSize[0])):
             j = 0
